7e347347efb1e377495b196b1a1e4726e23e275f
vaibhavii21 <vaibhavibhat14@gmail.com>
2023-02-25 20:27:55 +0530

    profile update

--- Front End/src/views/Profile/profile.js
+++ Front End/src/views/Profile/profile.js
@@ -11,7 +11,8 @@ import {
   CFormInput,
   CInputGroup,
   CInputGroupText,
-  CRow,
+  CInputGroupFile,
+  CRow, CImage
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilEnvelopeClosed, cilInfo, cilLockLocked, cilUser } from '@coreui/icons'
@@ -21,53 +22,81 @@ import Swal from 'sweetalert2'
 const profile = () => {
 
   const [userData, setUserData] = useState({
-    firstName : '',
-    lastName : '',
-    email : '',
+    firstName: '',
+    lastName: '',
+    email: '',
   })
-  
+
   const navigate = useNavigate()
 
   const onUpdate = () => {
-    if (!userData.firstName || !userData.lastName || !userData.email ) {
+    if (!userData.firstName || !userData.lastName || !userData.email) {
       Swal.fire({
         title: "Warning",
         text: "All fields are mandetory",
         icon: "warning",
-        confirmButtonColor: "#006f95"          
+        confirmButtonColor: "#006f95"
       })
       return
-    }  
+    }
   }
-  
-  
+
+
 
   return (
-    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+    <div className="bg-light min-vh-200 d-flex flex-row align-items-center">
       <CContainer>
         <CRow className="justify-content-center">
+
+
+
+
+
+
+
+          <CCol md={4}>
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <CForm> 
+                    <div className="min-vh-200">
+                      <CImage align="center" rounded src="/images/abc.jpg" width={200} height={200} />
+
+                      <b>Name</b> <p>Suraj</p>
+                      <b>Surname</b> <p>Jadhav</p>
+                      <b>Gmail</b> <p>suraj@gmail.com</p>
+                    </div>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+
+
           <CCol md={8}>
             <CCardGroup>
               <CCard className="p-4">
                 <CCardBody>
                   <CForm>
                     <h1>Profile</h1>
-                   
+
                     <CRow>
                       <CCol md={6}>
                         <CInputGroup className="mb-3">
                           <CInputGroupText>
                             <CIcon icon={cilUser} />
                           </CInputGroupText>
-                          <CFormInput placeholder="First Name" autoComplete="First Name" defaultValue={userData.firstName} onChange={e => setUserData({...userData, firstName : e.target.value})}/>
+                          <CFormInput placeholder="First Name" value="suraj" autoComplete="First Name" defaultValue={userData.firstName} onChange={e => setUserData({ ...userData, firstName: e.target.value })} />
                         </CInputGroup>
                       </CCol>
                       <CCol md={6} className="ml-0 pl-0">
                         <CInputGroup>
-                          <CFormInput placeholder="Last Name" autoComplete="Last Name" defaultValue={userData.lastName} onChange={e => setUserData({...userData, lastName : e.target.value})}/>
+                          <CFormInput placeholder="Last Name" value="Jadhav" autoComplete="Last Name" defaultValue={userData.lastName} onChange={e => setUserData({ ...userData, lastName: e.target.value })} />
                         </CInputGroup>
                       </CCol>
                     </CRow>
+
+
                     <CInputGroup className="mb-3">
                       <CInputGroupText>
                         <CIcon icon={cilEnvelopeClosed} />
@@ -75,24 +104,27 @@ const profile = () => {
                       <CFormInput
                         type="email"
                         placeholder="Email"
-                        defaultValue={userData.email} onChange={e => setUserData({...userData, email : e.target.value})}
+                        value="suraj@gmail.com"
+                        defaultValue={userData.email} onChange={e => setUserData({ ...userData, email: e.target.value })}
                       />
-                    
-                    
-                    
                     </CInputGroup>
+
+
+
+
+
                     <CRow>
                       <CCol xs={6}>
                         <CButton color="primary" >
                           Update
                         </CButton>
                       </CCol>
-                      
+
                     </CRow>
                   </CForm>
                 </CCardBody>
               </CCard>
-              
+
             </CCardGroup>
           </CCol>
         </CRow>
