928451d2adb6bf94e74ce037253a2585b798e524
vaibhavii21 <vaibhavibhat14@gmail.com>
2023-02-25 18:53:19 +0530

    profile

--- Front End/src/views/Profile/profile.js
+++ Front End/src/views/Profile/profile.js
@@ -1,36 +1,103 @@
-import { CContainer,CCol,CRow,CCard,CCardHeader,CForm,CCardBody,CImg } from "@coreui/react"
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilEnvelopeClosed, cilInfo, cilLockLocked, cilUser } from '@coreui/icons'
+import { createUser } from 'src/services/UserService'
+import Swal from 'sweetalert2'
+
 const profile = () => {
 
-    return<>
-     <CCard>
-            <CCardHeader>Profile</CCardHeader>
-            <CCardBody>
-                <CContainer>
-                    <CRow>
-                        <CCol md={3} className="border">
-                        <CCardBody>
-                        <CImg
-                          src="https://picsum.photos/1024/480/?image=54"
-                          fluid
-                          className="mb-2"
-                         />                                    
-                      </CCardBody>
-                        
-                        </CCol>
-                        <CCol md={9} className="border">
-                            <CForm className="row g-3 mt-1">
-                                <CCol xs="auto">
-                                    <CCardBody>
-                                                                      
-                                    </CCardBody>
-                                </CCol>
-                                </CForm>
-                                </CCol>
-                        </CRow>
-                        </CContainer>
-                        </CCardBody>
-            </CCard>
-    </>
-}
+  const [userData, setUserData] = useState({
+    firstName : '',
+    lastName : '',
+    email : '',
+  })
+  
+  const navigate = useNavigate()
 
+  const onUpdate = () => {
+    if (!userData.firstName || !userData.lastName || !userData.email ) {
+      Swal.fire({
+        title: "Warning",
+        text: "All fields are mandetory",
+        icon: "warning",
+        confirmButtonColor: "#006f95"          
+      })
+      return
+    }  
+  }
+  
+  
+
+  return (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={8}>
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <CForm>
+                    <h1>Profile</h1>
+                   
+                    <CRow>
+                      <CCol md={6}>
+                        <CInputGroup className="mb-3">
+                          <CInputGroupText>
+                            <CIcon icon={cilUser} />
+                          </CInputGroupText>
+                          <CFormInput placeholder="First Name" autoComplete="First Name" defaultValue={userData.firstName} onChange={e => setUserData({...userData, firstName : e.target.value})}/>
+                        </CInputGroup>
+                      </CCol>
+                      <CCol md={6} className="ml-0 pl-0">
+                        <CInputGroup>
+                          <CFormInput placeholder="Last Name" autoComplete="Last Name" defaultValue={userData.lastName} onChange={e => setUserData({...userData, lastName : e.target.value})}/>
+                        </CInputGroup>
+                      </CCol>
+                    </CRow>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        <CIcon icon={cilEnvelopeClosed} />
+                      </CInputGroupText>
+                      <CFormInput
+                        type="email"
+                        placeholder="Email"
+                        defaultValue={userData.email} onChange={e => setUserData({...userData, email : e.target.value})}
+                      />
+                    
+                    
+                    
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs={6}>
+                        <CButton color="primary" >
+                          Update
+                        </CButton>
+                      </CCol>
+                      
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+              
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
+}
 export default profile
