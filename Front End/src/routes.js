6e60cc4f4135aa8cafe968a8be67c67008599a52
vaibhavii21 <vaibhavibhat14@gmail.com>
2023-03-02 12:30:27 +0530

    checkout changes

--- Front End/src/routes.js
+++ Front End/src/routes.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Dashboard = React.lazy(() => import('./views/dashboard/UserDashboard'))
 const ManagerDashboard = React.lazy(() => import('./views/CanteenManager/ManagerDashboard'))
 const OrderFood = React.lazy(() => import('./views/orderFood/OrderFood'))
-const Logout = React.lazy(() => import('./views/auth/Logout'))
+//const Logout = React.lazy(() => import('./views/auth/Logout'))
 const CanteenManagerOrders = React.lazy(() => import('./views/CanteenManager/Orders'))
 const CanteenMenu = React.lazy(() => import('./views/CanteenManager/Menu'))
 const CanteenDiscount = React.lazy(() => import('./views/CanteenManager/Discount'))
@@ -11,6 +11,7 @@ const CanteenFeedback = React.lazy(() => import('./views/CanteenManager/Feedback
 const history = React.lazy(() => import('./views/History/history'))
 const profile = React.lazy(() => import('./views/Profile/profile'))
 const checkout = React.lazy(() => import('./views/checkout/checkout'))
+const Logout = React.lazy(() => import('./views/auth/Logout'))
 
 
 const routes = [
@@ -22,11 +23,13 @@ const routes = [
   { path: '/canteen-menu', name: 'Canteen Menu', element: CanteenMenu },
   { path: '/canteen-discount', name: 'Canteen Discount', element: CanteenDiscount },
   { path: '/canteen-feedback', name: 'Canteen Feedback', element: CanteenFeedback },
-  { path: '/logout', name: 'Logout', element: Logout}
-  { path: '/logout', name: 'Logout', element: Logout},
+  // { path: '/logout', name: 'Logout', element: Logout},
+  // { path: '/logout', name: 'Logout', element: Logout},
   { path: '/History', name: 'History', element: history},
   { path: '/Profile', name: 'Profile', element: profile },
   { path: '/checkout', name: 'checkout', element: checkout },
+   { path: '/logout', name: 'Logout', element: Logout},
+  { path: '/logout', name: 'Logout', element: Logout},
 
 ]
 
